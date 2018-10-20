0e1c16a861be555965ac4339ee54d47609c773aa
Igor Kramar <hombrehumor@gmail.com>
2018-10-20 20:16:00 +0600

    complete fullscreen menu

--- src/assets/scripts/modules/fullmenu.js
+++ src/assets/scripts/modules/fullmenu.js
@@ -1,10 +1,11 @@
 export default () => {
   const menuBtn = document.querySelector('.hamburger-btn');
   const menuWrapper = document.querySelector('.menu');
-  const header = document.querySelector('.main-header__top');
+  const body = document.querySelector('body');
 
   menuBtn.addEventListener('click', event => {
     menuBtn.classList.toggle('hamburger-btn--active');
     menuWrapper.classList.toggle('menu--show');
+    body.classList.toggle('noscroll');
   });
 }
