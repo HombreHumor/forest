a38e5aa786239b39781cdbeee2e38db2ffa68b34
Igor Kramar <hombrehumor@gmail.com>
2018-10-20 13:59:53 +0600

    fix adaptive blog

--- src/assets/scripts/works.js
+++ src/assets/scripts/works.js
@@ -5,3 +5,34 @@ import preloader from "./modules/preloader";
 preloader();
 slider();
 resizeSlider();
+
+
+let blur = (function () {
+  let wrapper = document.querySelector('.js-feedback'),
+    form = document.querySelector('.abouts__bg ');
+
+  return {
+    set: function () {
+      let imgTop = document.querySelector('.js-blur').offsetTop;
+      let wrapperTop = document.querySelector('.abouts__form').clientY;
+      let trY = imgTop - wrapperTop;
+      console.log(imgTop, wrapperTop);
+      form.style.transform = `translateY(-${trY}px)`;
+      // let imgWidth = document.querySelector('.js-blur').offsetWidth,
+      //   imgHeight = document.querySelector('.js-blur').offsetHeight,
+      //   posLeft = -wrapper.offsetLeft,
+      //   posTop = -wrapper.offsetTop,
+      //   blurCSS = form.style;
+      //   blurCSS.transform = `translateX(${posLeft})`;
+      // blurCSS.width = `${imgWidth}px`;
+      // blurCSS.height = `${imgHeight}px`;
+      // blurCSS.transform = `translate(${posLeft}px,${posTop}px)`;
+    },
+  };
+}());
+
+blur.set();
+
+window.onresize = function () {
+  blur.set();
+};
