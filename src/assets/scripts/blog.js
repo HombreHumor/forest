ed51fbd3a07ce580c86a1f1c52c1ef4972b224ef
Igor Kramar <hombrehumor@gmail.com>
2018-10-21 20:48:39 +0600

    add adaptive blog menu

--- src/assets/scripts/blog.js
+++ src/assets/scripts/blog.js
@@ -9,6 +9,9 @@ const articles = document.querySelectorAll('.blog__article');
 const blogMenu = document.querySelector('.blog__nav-list');
 const blog = document.querySelector('.blog__texts');
 const blogMenuBtns = document.querySelectorAll('.blog__nav-btn');
+const blogMenuSwipeBtns = document.querySelectorAll('.blog-swipe__nav-btn');
+const openSwipeBtn = document.querySelector('.blog-swipe__open-btn');
+const swipeMenu = document.querySelector('.blog-swipe');
 
 window.addEventListener('scroll', () => {
   stickyNav();
@@ -21,8 +24,19 @@ blogMenuBtns.forEach(element => {
   scrollToArticle(element);
 })
 
+blogMenuSwipeBtns.forEach(element => {
+  scrollToArticle(element);
+});
+
+openSwipeBtn.addEventListener('click', () => {
+  swipeMenu.classList.toggle('blog-swipe--active');
+});
+
 function scrollToArticle(element) {
   element.addEventListener('click', event => {
+    if (document.querySelector('.blog-swipe--active')) {
+      swipeMenu.classList.remove('blog-swipe--active');
+    }
     const scrollTarget = document.getElementById(event.target.dataset.scrollto);
     let scrollYCoord = scrollTarget.getBoundingClientRect().top;
     scrollBy(scrollYCoord, 1000);
@@ -31,19 +45,32 @@ function scrollToArticle(element) {
 
 function findActiveLink(element) {
   if (element.getBoundingClientRect().top <= 150) {
-    const targetLink = document.querySelector(`[data-scrollto='${element.id}']`);
-    setActiveLink(targetLink);
+    const targetLinks = document.querySelectorAll(`[data-scrollto='${element.id}']`);
+    targetLinks.forEach(btn => {
+      setActiveLink(btn);
+    });
   } 
 }
 
 function setActiveLink(targetBtn) {
-  blogMenuBtns.forEach(element => {
-    if (element == targetBtn) {
-      element.classList.add('js-blog-open-btn');
-    } else {
-      element.classList.remove('js-blog-open-btn');
-    }
-  });
+  if (targetBtn.dataset.type) {
+    blogMenuSwipeBtns.forEach(element => {
+      if (element === targetBtn) {
+        element.classList.add('blog-swipe__nav-btn--active');
+      } else {
+        element.classList.remove('blog-swipe__nav-btn--active');
+      }
+    });
+  } else {
+    blogMenuBtns.forEach(element => {
+      if (element == targetBtn) {
+        element.classList.add('js-blog-open-btn');
+      } else {
+        element.classList.remove('js-blog-open-btn');
+      }
+    });
+  }
+  
 }
 
 function stickyNav() {
